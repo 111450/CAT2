889349b8f73f004ab4f77eb43b8128cde402b2ea
111450 <151162768+111450@users.noreply.github.com>
2023-11-18 00:24:16 -0800

    config (correct)

--- config/DbConn.php
+++ config/DbConn.php
@@ -1,10 +1,17 @@
 <?php
-    require_once "CAT2/config/constants.php";
-    try{
-        $pdo = new PDO("mysql:host=$hostname; dbname=$dbname", $username, $userpass);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected Successfully!!";
-    } catch(PDOException $e){
-        echo "Connection failed: " . $e->getMessage();
-    }
+// Include the file with database constants
+include 'constants.php';
+
+try {
+    // Establishing a PDO connection without any additional security measures
+    $DbConn = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USERNAME, DB_PASSWORD);
+
+    // Simple error mode setting
+    $DbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connected successfully"; 
+} catch(PDOException $e) {
+    // Basic error handling
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
